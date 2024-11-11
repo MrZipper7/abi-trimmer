370a207e46ce8bfc6b2f92905c8b048d379d3fb1
MrZipper <mrzipperhots@gmail.com>
2024-11-11 09:19:50 -0500

    build: Update eslint config

--- src/utils/highlightSyntax.ts
+++ src/utils/highlightSyntax.ts
@@ -3,15 +3,15 @@ export function highlightSyntax(json: string, styles: CSSModuleClasses) {
     /("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\\-]?\d+)?)/g,
     match => {
       let cls = 'number'
-      if (/^"/.test(match)) {
-        if (/:$/.test(match)) {
+      if (match.startsWith("\"")) {
+        if (match.endsWith(":")) {
           cls = 'key'
         } else {
           cls = 'string'
         }
       } else if (/true|false/.test(match)) {
         cls = 'boolean'
-      } else if (/null/.test(match)) {
+      } else if (match.includes('null')) {
         cls = 'null'
       }
       return `<span class="${styles[cls]}">${match}</span>`
